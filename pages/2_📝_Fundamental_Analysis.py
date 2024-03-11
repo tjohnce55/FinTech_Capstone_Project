c1721c6918dfbc902a1179e1ca8343599630da46
tyler johnson <tjohnson8@css.edu>
2024-03-11 14:41:39 -0700

    Completed the stock news page and updated code.

--- pages/2_📝_Fundamental_Analysis.py
+++ pages/2_📝_Fundamental_Analysis.py
@@ -24,51 +24,74 @@ ticker = st.text_input("⬇️ Enter a Stock Ticker")
 
 # Only have the simulation run once the user clicks the "Submit" button.
 if st.button("Submit"):
-    # Create a variable that downloads data from yfinance using the ticker input.
-    stock_data = yf.Ticker(ticker)
-    
-    fundamentals = stock_data.info
-    
-    # Display the company's name as the header for the fundamentals that appear.
-    st.header(fundamentals["shortName"])
+    # Create a catch-all (using try and except statements) in case the user enters an invalid stock ticker.
+    try:
+        # Create a variable that downloads data from yfinance using the ticker input.
+        # Create a variable for just the fundamental data obtain from yfinance.
+        stock_data = yf.Ticker(ticker)
+        fundamentals = stock_data.info
 
-    # Display the metrics in three columns to fit the screen better on Streamlit.
-    col1, col2 = st.columns(2)
-    with col1:
-        st.metric("Sector", fundamentals["sector"])
-    with col2:
-        st.metric("Industry", fundamentals["industry"])
-    st.metric("Website", fundamentals["website"])
+        # Display the company's name as the header for the fundamentals that appear.
+        st.header(fundamentals["shortName"])
+
+        # Display the metrics in two columns to fit the screen better on Streamlit.
+        col1, col2 = st.columns(2)
+        with col1:
+            st.metric("Sector", fundamentals["sector"])
+        with col2:
+            st.metric("Industry", fundamentals["industry"])
+        st.metric("Website", fundamentals["website"])
+
+        # Create tabs that will obtain different data and tables.
+        business_summary, metrics, income_statement, balance_sheet, cash_flow, inst_holders = st.tabs(["Business Summary", "Metrics", "Income Statement", "Balance Sheet", "Cash Flow", "Institutional Holders"])
+
+        # Display the business summary within the "Business Summary" tab.
+        with business_summary:
+            st.write(fundamentals["longBusinessSummary"])
+
+        # Display various metrics within the "Metrics" tab.
+        # Create two columns to organize the metrics better.
+        # Round certain metrics to two decimal places and also write the ratio's formula.
+        with metrics:
+            column1, column2 = st.columns(2)
+            with column1:
+                market_cap = "{:,}".format(fundamentals["marketCap"])
+                st.metric("Market Cap:", market_cap)
+                dividend = round(fundamentals["dividendYield"] * 100, 2)
+                st.metric("Dividend Yield: (%)", dividend)
+                st.metric("Beta:", fundamentals["beta"])
+                trailing_pe = round(fundamentals["trailingPE"], 2)
+                st.metric("Trailing PE: (Share Price / Recent EPS)", trailing_pe)
+                price_to_book = round(fundamentals["priceToBook"], 2)
+                st.metric("Price-To-Book", price_to_book)
+                st.metric("Yahoo Finance Recommendation:", fundamentals["recommendationKey"])
+            with column2:
+                quick_ratio = round(fundamentals["quickRatio"], 2)
+                st.metric("Quick Ratio: (Liquid Assets / Current Liabilities)", quick_ratio)
+                current_ratio = round(fundamentals["currentRatio"], 2)
+                st.metric("Current Ratio: (Current Assets / Current Liabilities)", current_ratio)
+                debt_to_equity = round(fundamentals["debtToEquity"], 2)
+                st.metric("Debt-To-Equity: (Total Debt / Total Shareholder Equity)", debt_to_equity)
+                return_on_equity = round(fundamentals["returnOnEquity"], 2)
+                st.metric("Return on Equity: (Net Income / Avg Shareholder Equity)", return_on_equity)
+                st.metric("Target Mean Price:", fundamentals["targetMeanPrice"]) 
+
+        # Display the annual income statements for the last four years within the "Income Statement" tab.
+        with income_statement:
+            st.dataframe(stock_data.income_stmt)
+
+        # Display the annual balance sheets for the last four years within the "Balance Sheet" tab.
+        with balance_sheet:
+            st.dataframe(stock_data.balance_sheet)
+
+        # Display the annual cash flow statements for the last four years within the "Cash Flow" tab.
+        with cash_flow:
+            st.dataframe(stock_data.cashflow)
+
+        # Display the current institutional holders within the "Institutional Holders" tab.
+        with inst_holders:
+            st.dataframe(stock_data.institutional_holders)
         
-    business_summary, metrics, income_statement, balance_sheet, cash_flow, inst_holders = st.tabs(["Business Summary", "Metrics", "Income Statement", "Balance Sheet", "Cash Flow", "Institutional Holders"])
-    
-    with business_summary:
-        st.write(fundamentals["longBusinessSummary"])
-        
-    with metrics:
-        column1, column2 = st.columns(2)
-        with column1:
-            st.metric("Market Cap", fundamentals["marketCap"])
-            st.metric("Dividend Yield", fundamentals["dividendYield"])
-            st.metric("Beta", fundamentals["beta"])
-            st.metric("Trailing PE", fundamentals["trailingPE"])
-            st.metric("Price-To-Book", fundamentals["priceToBook"])
-            st.metric("Yahoo Finance Recommendation", fundamentals["recommendationKey"])
-        with column2:
-            st.metric("Quick Ratio", fundamentals["quickRatio"])
-            st.metric("Current Ratio", fundamentals["currentRatio"])
-            st.metric("Deb-To-Equity", fundamentals["debtToEquity"])
-            st.metric("Return on Equity", fundamentals["returnOnEquity"])
-            st.metric("Target Mean Price", fundamentals["targetMeanPrice"]) 
-        
-    with income_statement:
-        st.dataframe(stock_data.income_stmt)
-        
-    with balance_sheet:
-        st.dataframe(stock_data.balance_sheet)
-        
-    with cash_flow:
-        st.dataframe(stock_data.cashflow)
-        
-    with inst_holders:
-        st.dataframe(stock_data.institutional_holders)
+    # Display a warning message that appears when the user enters an invalid stock ticker.
+    except:
+        st.warning("Oops, please enter a valid stock ticker!")
--- pages/4_📰_Stock_News.py
+++ pages/4_📰_Stock_News.py
@@ -1,22 +1,46 @@
+"""
+This page will allow the user to enter a stock ticker and obtain some of the most recent news articles relating to that company.
+
+When an error occurs, or exception, Python will stop and generate an error message.
+We will handle these exceptions using the try and except statements.
+If the user enters an invalid ticker they will be asked to reenter a valid ticker rather than Python giving them an error message.
+"""
+
+# Import the required libraries and dependencies.
 import streamlit as st
-import pandas as pd
-import numpy as np
 import yfinance as yf
-import requests
 
-from stocknews import StockNews
+# Display a title for the the stock news page.
+st.title("📰 Stock News Articles")
 
-st.title(":boom: Stock News Articles :boom:")
+# Display a subheader that explains the tool.
+st.write("Enter a ticker below. The tool will obtain and display recent news articles for the company of your choice.")
 
-# Create a text input for the stock ticker
-ticker = st.text_input("Stock Ticker")
+st.divider()
 
-sn = StockNews(ticker, save_news = False)
-df =sn.read_rss()
+# Create a text input for the user to enter a stock ticker.
+ticker = st.text_input("⬇️ Enter a Stock Ticker")
 
+# Only have the simulation run once the user clicks the "Give Me News" button.
 if st.button("Give Me News"):
-    for i in range(10):
-        st.header(df["title"][i])
-        st.subheader(df["published"][i])
-        st.write(df["summary"][i])
+    # Create a catch-all (using try and except statements) in case the user enters an invalid stock ticker.
+    try:
+        # Create a variable that downloads data from yfinance using the ticker input.
+        # Create a variable for just the stock news from yfinance.
+        stock_data = yf.Ticker(ticker)
+        stock_news = stock_data.news
         
+        # Display a header for the news articles that appear.
+        st.header(f"{ticker} News:")
+        
+        # Loop through the articles and display the titles, publishers, and article links.
+        for i in range(8):
+            st.subheader(f"Article {i+1}")
+            st.write("Title: ", stock_news[i]["title"])
+            st.write("Publisher: ", stock_news[i]["publisher"])
+            st.write("Article Link: ", stock_news[i]["link"])
+            st.divider()
+    
+    # Display a warning message that appears when the user enters an invalid stock ticker.
+    except:
+        st.warning("Oops, please enter a valid stock ticker!")
